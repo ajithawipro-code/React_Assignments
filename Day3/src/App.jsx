f8ab72841301c25d57787d440748051c3d9af626
Ajitha <ajitha.wipro@gmail.com>
2025-12-15 22:42:13 +0530

    proper first react code

---
+++ Day3/src/App.jsx
@@ -0,0 +1,18 @@
+import Counter from "./Counter";
+
+function App(){
+
+
+  return (
+    
+      <div>
+
+        <Counter />
+        
+      </div>
+    
+  )
+
+}
+
+export default App;
---
+++ First_React_Assign/src/UseRefComponent.jsx
@@ -0,0 +1,15 @@
+import react from "react"
+
+function UseRefComponent(){
+
+
+    return (
+        <>
+
+        <input></input>
+        <button></button>
+    </>
+    )
+}
+
+export default UseRefComponent
