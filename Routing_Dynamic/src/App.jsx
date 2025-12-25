2e6a3f61ad2766966243c9da7adbbcd4d6073283
Ajitha <ajitha.wipro@gmail.com>
2025-12-25 22:34:48 +0530

    dynamic routing

---
+++ Routing_Dynamic/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Todos from "./pages/Todos";
+import TodoDetails from "./pages/TodoDetails";
+import ProtectedRoute from "./ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/todos"
+          element={
+            <ProtectedRoute>
+              <Todos />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/todos/:todoId"
+          element={
+            <ProtectedRoute>
+              <TodoDetails />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
